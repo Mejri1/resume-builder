26f8660fd9786329a02dd1dc1bc5606baae39c85
Mejri1 <omarmejri@etudiant-fst.utm.tn>
2025-02-10 10:48:09 +0100

    Use environment variables for Google Auth

--- pages/api/generate-pdf.js
+++ pages/api/generate-pdf.js
@@ -255,19 +255,10 @@ async function generatePdf(html) {
 // Fonction pour uploader le PDF sur Google Drive
 async function uploadToGoogleDrive(filePath, resumeData, photoPath) {
   const auth = new google.auth.GoogleAuth({
-    credentials: {
-      type: "service_account",
-      project_id: process.env.GOOGLE_PROJECT_ID,
-      private_key: process.env.GOOGLE_PRIVATE_KEY.replace(/\\n/g, "\n"),
-      client_email: process.env.GOOGLE_CLIENT_EMAIL,
-      client_id: process.env.GOOGLE_CLIENT_ID,
-      auth_uri: process.env.GOOGLE_AUTH_URI,
-      token_uri: process.env.GOOGLE_TOKEN_URI,
-      auth_provider_x509_cert_url: process.env.GOOGLE_AUTH_PROVIDER_CERT_URL,
-      client_x509_cert_url: process.env.GOOGLE_CLIENT_CERT_URL,
-    },
+    credentials: JSON.parse(process.env.GOOGLE_CREDENTIALS),
     scopes: ["https://www.googleapis.com/auth/drive.file"],
-  });
+});
+
 
   const drive = google.drive({ version: "v3", auth });
 
